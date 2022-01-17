e4570d8329f2a6f53cefa55c5bf5201fc61d40eb
Navarrete <aaldiitoo@gmail.com>
2022-01-17 12:50:18 -0600

    Letting one navbar TODO:modify

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -7,25 +7,19 @@ export const Navbar = () => {
     const handleLogout = () => {
         navigate('/login', { replace: true });
     }   
-
+    //TODO: Modify the navbar toggler
     return (
         <div className="hello">
             <nav className="navbar navbar-expand-lg navbar-light"  style={{backgroundColor: '#e1e5e9'}}>
-                <div className="container">
-                    <Link className="navbar-brand" to="/">
-                    <img src="/assets/marvel-dc-logo.png" alt="" width="50" height="60"/>
-                    </Link>
-                    <Link
-                        className="navbar-brand"
-                        to="/"
-                    >
-                        Asociaciones
-                    </Link>
-                <form className="d-flex">
-                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                    <button className="btn btn-outline-success" type="submit">Search</button>
-                </form>
-                </div>
+                <Link className="navbar-brand" to="/">
+                <img src="/assets/marvel-dc-logo.png" alt="" width="50" height="60"/>
+                </Link>
+                <Link
+                    className="navbar-brand"
+                    to="/"
+                >
+                    Asociaciones
+                </Link>
                 <div className="collapse navbar-collapse">
                     <div className="navbar-nav">
                         <NavLink
@@ -40,6 +34,23 @@ export const Navbar = () => {
                         >
                             DC
                         </NavLink>
+                        <NavLink
+                            className={({isActive})=>"nav-item nav-link " + (isActive ? " active" : "")}
+                            to="/search"
+                        >
+                            Search
+                        </NavLink>
+                        <button
+                          className="navbar-toggler"
+                          type="button"
+                          data-bs-toggle="collapse"
+                          data-bs-target="#navbarSupportedContent"
+                          aria-controls="navbarSupportedContent"
+                          aria-expanded="false"
+                          aria-label="Toggle navigation"
+                        >
+                          <span className="navbar-toggler-icon" />
+                        </button>
                     </div>
                 </div>
                 <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
@@ -55,91 +66,22 @@ export const Navbar = () => {
                     </ul>
                 </div>
             </nav>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">
-      Navbar
-    </a>
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon" />
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">
-            Home
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">
-            Link
-          </a>
-        </li>
-        <li className="nav-item dropdown">
-          <a
-            className="nav-link dropdown-toggle"
-            href="#"
-            id="navbarDropdown"
-            role="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Dropdown
-          </a>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li>
-              <a className="dropdown-item" href="#">
-                Action
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Another action
-              </a>
-            </li>
-            <li>
-              <hr className="dropdown-divider" />
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Something else here
-              </a>
-            </li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a
-            className="nav-link disabled"
-            href="#"
-            tabIndex={-1}
-            aria-disabled="true"
-          >
-            Disabled
-          </a>
-        </li>
-      </ul>
-      <form className="d-flex">
-        <input
-          className="form-control me-2"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-        />
-        <button className="btn btn-outline-success" type="submit">
-          Search
-        </button>
-      </form>
-    </div>
-  </div>
-</nav>
+            {/* <nav class="navbar navbar-expand-lg navbar-light bg-light">
+              <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                  <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="#">Features</a>
+                    <a class="nav-link" href="#">Pricing</a>
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                  </div>
+                </div>
+              </div>
+            </nav> */}
 
         </div>
 
